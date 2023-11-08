46d8de0bf9246691554fb37f856b1cc34c9c4e2d
Arturo-Blanco <arturoblanco@tdf.edu.ar>
2023-11-08 17:02:28 -0300

    Database seeding is added

---
+++ TaskList/DataAccess/DatabaseSeeding/IEntitySeeder.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskList.DataAccess.DatabaseSeeding
+{
+    public interface IEntitySeeder
+    {
+        public void SeedDataBase(ModelBuilder modelBuilder) 
+        { }
+    }
+}
---
+++ TaskList/DataAccess/DatabaseSeeding/TodoSeeder.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskList.DataAccess.DatabaseSeeding
+{
+    public class TodoSeeder : IEntitySeeder
+    {
+        public void SeedDataBase(ModelBuilder modelBuilder)
+        { }
+    }
+}
